84feeb5fcfeb757c2d79fbffc64e4a472770413a
Shree Sen <147225217+askshreesen@users.noreply.github.com>
2025-07-25 12:26:38 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
+export const metadata = {
+  title: 'Coinflip',
+  description: 'Simple coin flipping app',
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <head />
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
